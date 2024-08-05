de976bd8bdfaa3976ee2bd7b28d1b2457b4b1059
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-08-05 16:38:06 +0530

    add db

--- server/app.js
+++ server/app.js
@@ -1,5 +1,22 @@
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
 const express =require('express');
 const app=express();
+
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+
+const PORT=process.env.PORT;
+
+
+
+//middleware
+
+const middleware=(req,res,next)=>{
+console.log('hello middleware');
+next();
+
+};
 
 
 app.get('/',(req,res)=>{
@@ -7,7 +24,7 @@ res.send(`hello world from the server`)
 });
 
 // about me
-app.get('/about',(req,res)=>{
+app.get('/about',middleware,(req,res)=>{
     res.send('aboutme');
 });
 
@@ -28,7 +45,7 @@ app.get('/signup',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`server is running at port 3000`);
     
 })
