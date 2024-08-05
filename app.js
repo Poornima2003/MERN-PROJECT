7b4c7767c5b6d70f61598792312ad75900bb1709
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-08-05 15:13:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express =require('express');
+const app=express();
+
+
+app.get('/',(req,res)=>{
+res.send(`hello world from the server`)
+});
+
+// about me
+app.get('/about',(req,res)=>{
+    res.send('aboutme');
+});
+
+//contact
+app.get('/contact',(req,res)=>{
+    res.send('contact form');
+})
+
+//login
+app.get('/signin',(req,res)=>{
+    res.send('login here');
+});
+
+//register
+app.get('/signup',(req,res)=>{
+    res.send("register your account herrr")
+})
+
+
+app.listen(3000,()=>{
+    console.log(`server is running at port 3000`);
+    
+})
+
