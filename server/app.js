9cabca6442157cb62faa5fc6aed4a9eb473ef9c4
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-08-06 22:47:39 +0530

    add

--- server/app.js
+++ server/app.js
@@ -5,7 +5,15 @@ const app=express();
 
 dotenv.config({path:'./config.env'});
 require('./db/conn');
+
+
+app.use(express.json());
+
 const User=require('./model/userSchema');
+
+//we link the router folder
+app.use(require('./router/auth'));
+
 
 const PORT=process.env.PORT;
 
---
+++ server/router/auth.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const router=express.Router();
+require('../db/conn');
+const User=require("../model/userSchema");
+
+
+router.get('/',(req,res)=>{
+    res.send('hello from the router server auth.js')
+});
+
+router.post('/register',async (req,res)=>{
+    const {name,email,phone,work,password,cpassword}=req.body;
+if(!name || !email|| !phone || !work || !password || !cpassword){
+    return res.status(422).json({error: "plz give correct data"});
+    
+}
+User.findOne({email:email})
+.then((userExist)=>{
+    if(userExist){
+        return res.status(422).json({error:"email already exist"});
+    }
+    const user=new User({name,email,phone,work,password,cpassword
+
+    });
+    user.save().then(()=>{
+        res.status(201).json({message:"user register sucessfully"})
+
+    }).catch((err)=>{
+          res.status(500).json({error:"failed to registered"})
+    }).catch(err=>{
+      console.log(err);
+      
+    })
+
+})
+
+
+});
+
+
+module.exports=router;
